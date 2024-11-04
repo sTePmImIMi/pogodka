28758196d1cd1013bdefdb8eda1cc2f1df9bf131
bdsm <36733200+sTePmImIMi@users.noreply.github.com>
2024-11-04 21:19:36 +0300

    визуал

--- app/src/main/java/com/example/weather/fragments/Extensions.kt
+++ app/src/main/java/com/example/weather/fragments/Extensions.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
+//проверка на уже существующее разрешение
 fun Fragment.isPermissionGranted(permission: String): Boolean {
     return ContextCompat.checkSelfPermission( //передает digit, а не boolean
         activity as AppCompatActivity, permission) == PackageManager.PERMISSION_GRANTED
--- app/src/main/java/com/example/weather/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weather/fragments/MainFragment.kt
@@ -1,16 +1,20 @@
 package com.example.weather.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.weather.R
 import com.example.weather.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String> //лаунчер для разрешений
     private lateinit var binding: FragmentMainBinding
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,9 +23,37 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) { //проверка
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission_launch()
+    }
+
+    private fun checkPermission_launch() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION) //запускает запрос разрешения на доступ к геолокации у пользователя.
+        }
+
+    }
+
+    private fun permissionListener() {
+        //проверка в реальном времени
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ //настраивается обработчик запроса разрешения, чтобы можно было обработать результат запроса.
+            Toast.makeText(activity,"permission is :$it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
+
+
     companion object {
         @JvmStatic
         fun newInstance() =
             MainFragment()
     }
+
+
+
 }
